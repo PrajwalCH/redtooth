5999b40ee637a3d88339cd3952f618ed0f7d9609
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-07-01 20:52:53 +0545

    feat(main): Start receving data on TCP/IP once app starts
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::collections::{hash_map::DefaultHasher, HashMap};
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
-use std::io;
-use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
+use std::io::{self, Read, Write};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream, UdpSocket};
 use std::sync::mpsc::{self, Sender};
 use std::thread;
 
@@ -10,17 +11,21 @@ use std::thread;
 const MULTICAST_ADDRESS: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 251);
 const MULTICAST_PORT: u16 = 20581;
 const ANY_INTERFACE_ADDRESS: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
+const TCP_PORT: u16 = 25802;
 
 type DeviceId = u64;
 type DeviceAddress = SocketAddr;
 
 struct Group {
+    /// Current device address.
+    device_address: DeviceAddress,
     joined_devices: HashMap<DeviceId, DeviceAddress>,
 }
 
 impl Group {
     pub fn new() -> Self {
         Self {
+            device_address: DeviceAddress::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), TCP_PORT),
             joined_devices: HashMap::new(),
         }
     }
@@ -52,9 +57,14 @@ impl Group {
 
         loop {
             let mut inbox = [0; 12];
-            let Ok((_, device_address)) = socket.recv_from(&mut inbox) else {
+            let Ok((_, mut device_address)) = socket.recv_from(&mut inbox) else {
                 continue;
             };
+
+            // All the devices receive data on a fixed `TCP_PORT` but the address currently we are
+            // receiving may have different port because they announce themselves using the port
+            // chosen by OS or a specifically `UNSPECIFIED` port.
+            device_address.set_port(TCP_PORT);
 
             if inbox != "announcement".as_bytes() {
                 continue;
@@ -79,14 +89,48 @@ fn main() -> io::Result<()> {
     let (sender, receiver) = mpsc::channel::<(DeviceId, DeviceAddress)>();
     let builder = thread::Builder::new().name(String::from("announcer"));
     builder.spawn(move || Group::listen_new_announcement(&sender))?;
+
+    let mut group = Group::new();
     // Announce self to other group server instances.
     Group::announce()?;
 
-    let mut group = Group::new();
+    let listener = TcpListener::bind(group.device_address)?;
+    println!("[Main]: Receiving data on: {}", listener.local_addr()?);
+
+    let builder = thread::Builder::new().name(String::from("data receiver"));
+
+    builder.spawn(move || {
+        for peer_stream in listener.incoming() {
+            let Ok(mut peer_stream) = peer_stream else {
+                continue;
+            };
+
+            // NOTE: For now the buffer is only used for holding `ping` message.
+            let mut data_buffer = [0; 6];
+            peer_stream.read_exact(&mut data_buffer).ok();
+
+            let data = std::str::from_utf8(&data_buffer).unwrap();
+            let peer_address = peer_stream.peer_addr().unwrap();
+
+            if !data_buffer.is_empty() {
+                println!("[Main]: Received `{data}` from {peer_address}");
+            }
+        }
+    })?;
 
     loop {
+        // NOTE: `recv` blocks the current thread so, we can't send the data while we are waiting
+        //       to receive newly joined device sent from the `announcer` thread.
         if let Ok((device_id, device_address)) = receiver.recv() {
             group.add_new_device(device_id, device_address);
+        }
+
+        // Send ping message to all the devices.
+        for peer_address in group.joined_devices.values() {
+            println!("[Main]: Sending `ping` to {peer_address}");
+
+            let mut peer_stream = TcpStream::connect(peer_address)?;
+            peer_stream.write_all("ping".as_bytes())?;
         }
     }
 }
