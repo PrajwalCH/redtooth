43a0dd54b2825577847b387ef099980ffa3b33c9
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-07-22 11:57:27 +0545

    feat(main): No longer needed to run event loop on different thread
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,9 @@ mod discovery_server;
 mod interface;
 mod macros;
 
-use std::thread;
-
-use crate::app::{App, Event};
+use crate::app::App;
 
 fn main() -> std::io::Result<()> {
     let mut app = App::new();
-    let event_emitter = app.event_emitter();
-
-    let event_loop_thread = thread::Builder::new()
-        .name(String::from("event loop"))
-        .spawn(move || app.run())?;
-
-    loop {
-        if event_loop_thread.is_finished() {
-            event_loop_thread.join().unwrap()?;
-            break;
-        }
-        event_emitter.emit(Event::PingAll);
-        thread::sleep(std::time::Duration::from_secs(2));
-    }
-    Ok(())
+    app.run()
 }
