d47d0fd7f6dcd94c90e61687a8a32153dedd1b87
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-07-29 15:19:12 +0545

    refactor(local): Use short form of `address` i.e. `addr`
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/peer_discoverer/local.rs
+++ src/peer_discoverer/local.rs
@@ -11,7 +11,7 @@ use crate::{elogln, logln};
 
 // Range between `224.0.0.0` to `224.0.0.250` is reserved or use by routing and maintenance
 // protocols inside a network.
-const MULTICAST_ADDRESS: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 251);
+const MULTICAST_ADDR: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 251);
 const MULTICAST_PORT: u16 = 20581;
 
 /// Spawns a local server.
@@ -21,13 +21,13 @@ pub fn spawn(peer_map: Arc<Mutex<PeerMap>>) -> io::Result<ThreadHandle> {
 }
 
 /// Announces the peer to other instances of the local server.
-pub fn announce_peer(id: PeerID, address: PeerAddr) -> io::Result<()> {
+pub fn announce_peer(id: PeerID, addr: PeerAddr) -> io::Result<()> {
     let socket = UdpSocket::bind("0.0.0.0:0")?;
     // Don't announce to the current instance of the server.
     socket.set_multicast_loop_v4(false)?;
 
-    let packet = format!("{id};{address}");
-    socket.send_to(packet.as_bytes(), (MULTICAST_ADDRESS, MULTICAST_PORT))?;
+    let packet = format!("{id};{addr}");
+    socket.send_to(packet.as_bytes(), (MULTICAST_ADDR, MULTICAST_PORT))?;
     Ok(())
 }
 
@@ -35,23 +35,23 @@ pub fn announce_peer(id: PeerID, address: PeerAddr) -> io::Result<()> {
 fn discover_peers(peer_map: Arc<Mutex<PeerMap>>) -> io::Result<()> {
     let socket = UdpSocket::bind(("0.0.0.0", MULTICAST_PORT))?;
     // socket.set_read_timeout(Some(Duration::from_millis(20)))?;
-    socket.join_multicast_v4(&MULTICAST_ADDRESS, &Ipv4Addr::UNSPECIFIED)?;
+    socket.join_multicast_v4(&MULTICAST_ADDR, &Ipv4Addr::UNSPECIFIED)?;
     logln!("Listening for new announcement on {}", socket.local_addr()?);
 
     loop {
         let mut packet = [0; 4096];
-        let Ok((packet_len, announcement_address)) = socket.recv_from(&mut packet) else {
+        let Ok((packet_len, announcement_addr)) = socket.recv_from(&mut packet) else {
             continue;
         };
-        let Some((id, mut address)) = parse_packet(&packet[..packet_len]) else {
-            elogln!("Received a badly formatted packet from {announcement_address}");
+        let Some((id, mut addr)) = parse_packet(&packet[..packet_len]) else {
+            elogln!("Received a badly formatted packet from {announcement_addr}");
             continue;
         };
 
         // If the address present in a packet is unspecified (0.0.0.0), use the address from
         // which the peer announces itself.
-        if address.ip().is_unspecified() {
-            address.set_ip(announcement_address.ip());
+        if addr.ip().is_unspecified() {
+            addr.set_ip(announcement_addr.ip());
         }
 
         // Unlock the map's lock ASAP using inner block.
@@ -64,9 +64,9 @@ fn discover_peers(peer_map: Arc<Mutex<PeerMap>>) -> io::Result<()> {
                     continue;
                 }
             };
-            peer_map.insert(id, address);
+            peer_map.insert(id, addr);
         }
-        logln!("Discovered `{address}`");
+        logln!("Discovered `{addr}`");
     }
 }
 
@@ -80,6 +80,6 @@ fn parse_packet(packet: &[u8]) -> Option<(PeerID, PeerAddr)> {
     let mut content_iter = packet.split(';');
 
     let id = content_iter.next()?.parse::<PeerID>().ok()?;
-    let address = content_iter.next()?.parse::<PeerAddr>().ok()?;
-    Some((id, address))
+    let addr = content_iter.next()?.parse::<PeerAddr>().ok()?;
+    Some((id, addr))
 }
