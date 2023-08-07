591a6fbec5b905493416ed365eb9abe6a9495e67
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-08-07 20:40:25 +0545

    refactor(protocol): Include doc comment for `FilePacket` methods
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/protocol.rs
+++ src/protocol.rs
@@ -149,7 +149,7 @@ impl<'p> Packet<'p> {
 pub struct FilePacket<'data>(Packet<'data>);
 
 impl<'data> FilePacket<'data> {
-    /// Creates a new empty file packet.
+    /// Creates a new file packet by reading a file from the given path.
     pub fn from_path(path: &Path) -> io::Result<FilePacket<'data>> {
         let file_name = path.file_name().unwrap_or(path.as_os_str());
         let file_contents = fs::read(path)?;
@@ -160,18 +160,22 @@ impl<'data> FilePacket<'data> {
         Ok(FilePacket(packet))
     }
 
+    /// Creates a new file packet by parsing the given bytes.
     pub fn from_bytes(bytes: &[u8]) -> Result<FilePacket, PacketParseError> {
         Ok(FilePacket(Packet::from_bytes(bytes)?))
     }
 
+    /// Returns the name of the file, if available; otherwise returns default (`undefined`).
     pub fn get_file_name(&self) -> &str {
         self.0.get_header("file_name").unwrap_or("undefined")
     }
 
+    /// Returns the contents of the file, if available; otherwise returns empty.
     pub fn get_contents(&self) -> &[u8] {
         self.0.get_payload().unwrap_or_default()
     }
 
+    /// Converts the packet into a bytes which can be sent over the network.
     pub fn as_owned_bytes(&self) -> Vec<u8> {
         self.0.as_bytes()
     }
