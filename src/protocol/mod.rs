97b5f8a62b8358b80cd03cf268100e522c0ba778
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-08-10 18:26:28 +0545

    refactor(protocol): Rename `TCP_PORT` to `DEFAULT_PEER_PORT`
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -7,7 +7,7 @@ use std::time::Instant;
 
 use crate::interface;
 
-const TCP_PORT: u16 = 25802;
+const DEFAULT_PEER_PORT: u16 = 25802;
 
 pub type PeerID = u64;
 pub type PeerAddr = SocketAddr;
@@ -20,5 +20,5 @@ pub fn get_my_id() -> PeerID {
 
 pub fn get_my_addr() -> PeerAddr {
     let ip_addr = IpAddr::V4(interface::local_ipv4_address().unwrap_or(Ipv4Addr::UNSPECIFIED));
-    PeerAddr::new(ip_addr, TCP_PORT)
+    PeerAddr::new(ip_addr, DEFAULT_PEER_PORT)
 }
