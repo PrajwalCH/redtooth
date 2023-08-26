09c2195fafa76313cdb50daea9ffe4710bea7d61
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-08-26 09:18:06 +0545

    refactor(discovery): Move `Announcement` to its own `announcement` module
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/discovery/mod.rs
+++ src/discovery/mod.rs
@@ -1,11 +1,12 @@
+mod announcement;
 mod local;
 
 use std::collections::HashMap;
+use std::io;
 use std::sync::{Arc, Mutex};
 use std::thread::JoinHandle;
-use std::{fmt, io};
 
-use crate::protocol::packet::{InvalidHeaderSequence, Packet};
+use self::announcement::Announcement;
 use crate::protocol::{PeerAddr, PeerID};
 
 type PeerMap = HashMap<PeerID, PeerAddr>;
@@ -60,56 +61,5 @@ impl PeerDiscoverer {
             .lock()
             .ok()
             .and_then(|peer_map| peer_map.get(&id).copied())
-    }
-}
-
-enum InvalidAnnouncement {
-    MissingPeerID,
-    MissingPeerAddr,
-    InvalidPacket(InvalidHeaderSequence),
-}
-
-impl fmt::Display for InvalidAnnouncement {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use self::InvalidAnnouncement::*;
-
-        match self {
-            MissingPeerID => write!(f, "missing peer id"),
-            MissingPeerAddr => write!(f, "missing peer address"),
-            InvalidPacket(e) => write!(f, "invalid packet: {e}"),
-        }
-    }
-}
-
-struct Announcement {
-    peer_id: PeerID,
-    peer_addr: PeerAddr,
-}
-
-impl Announcement {
-    pub fn new(peer_id: PeerID, peer_addr: PeerAddr) -> Announcement {
-        Announcement { peer_id, peer_addr }
-    }
-
-    pub fn from_bytes(bytes: &[u8]) -> Result<Announcement, InvalidAnnouncement> {
-        let packet = Packet::from_bytes(bytes).map_err(InvalidAnnouncement::InvalidPacket)?;
-
-        let peer_id = packet
-            .get_header("id")
-            .and_then(|id| id.parse::<PeerID>().ok())
-            .ok_or(InvalidAnnouncement::MissingPeerID)?;
-        let peer_addr = packet
-            .get_header("addr")
-            .and_then(|addr| addr.parse::<PeerAddr>().ok())
-            .ok_or(InvalidAnnouncement::MissingPeerAddr)?;
-
-        Ok(Announcement { peer_id, peer_addr })
-    }
-
-    pub fn as_bytes(&self) -> Vec<u8> {
-        let mut packet = Packet::new();
-        packet.set_header("id", self.peer_id);
-        packet.set_header("addr", self.peer_addr);
-        packet.as_bytes()
     }
 }
